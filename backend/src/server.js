dc7b147de7c59e137f79dc085ded3fad5998a36a
Sneha-Naraniwal <150178875+Sneha-Naraniwal@users.noreply.github.com>
2026-02-27 14:43:32 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -16,11 +16,11 @@ app.get("/",(req, res)=>{
 })
 ////f app for deployment
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname,"../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
+  app.get(/.*/, (req, res) => {
     res.sendFile(
-      path.join(__dirname, "../frontend", "dist", "index.html")
+      path.join(__dirname, "../frontend/dist/index.html")
     );
   });
 }
