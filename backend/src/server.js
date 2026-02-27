df6b2388a3e174fe2c70c52666efee67275c0f32
Sneha-Naraniwal <150178875+Sneha-Naraniwal@users.noreply.github.com>
2026-02-27 14:31:47 +0530

    Fix environment variable check for production

--- backend/src/server.js
+++ backend/src/server.js
@@ -15,7 +15,7 @@ app.get("/",(req, res)=>{
   res.status(200).json({msg:" Success from api"})
 })
 ////f app for deployment
-if (ENV.NODE_ENV === "production") {
+if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname,"../frontend/dist")));
 
   app.get("/*any", (req, res) => {
