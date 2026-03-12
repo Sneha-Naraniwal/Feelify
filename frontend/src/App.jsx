ab269ece78a599928d1abbecfdeb0642c6cf9a81
mitul-2210 <tandonmitul277@gmail.com>
2026-03-12 14:17:00 +0530

    updated frontend pages and assets

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,36 @@
-
 import {
   SignedIn,
   SignedOut,
   SignInButton,
   SignOutButton,
-  UserButton
+  UserButton,
+  useUser,
 } from "@clerk/clerk-react";
-import {Route , Routes} from 'react-router';
+import { Navigate, Route, Routes } from "react-router";
+import HomePage from "./pages/HomePage";
+
+import ProblemsPage from "./pages/ProblemsPage";
+import { Toaster } from "react-hot-toast";
+import DashboardPage from "./pages/DashboardPage";
 
 function App() {
+  const { isSignedIn, isLoaded } = useUser();
+
+  // this will get rid of the flickering effect
+  if (!isLoaded) return null;
+
   return (
     <>
-      <h1>Welcome to the Major Project</h1>
+      <Routes>
+        <Route path="/" element={!isSignedIn ? <HomePage /> : <Navigate to={"/dashboard"} />} />
+        <Route path="/dashboard" element={isSignedIn ? <DashboardPage /> : <Navigate to={"/"} />} />
 
-      <SignedOut>
-        <SignInButton mode="modal" />
-      </SignedOut>
+        <Route path="/problems" element={isSignedIn ? <ProblemsPage /> : <Navigate to={"/"} />} />
+      </Routes>
 
-      <SignedIn>
-        <SignOutButton />
-      </SignedIn>
-      <UserButton />
+      <Toaster toastOptions={{ duration: 3000 }} />
     </>
-  )
+  );
 }
 
 export default App;
